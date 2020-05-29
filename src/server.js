d076cab657643916a2a3f9c0e1660c7b456e2349
pswilso2017 <pswilso@us.ibm.com>
2020-05-29 11:31:56 -0400

    changes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(req,res){
 
 	var url = req.url
 
-	var hello = "Hello, here is some information about your request to the web server";
+	var hello = "Hello There!, here is some information about your request to the web server";
 	var info = "The url requested was: " + url
 
 	var returnString = hello + '\n' + info
